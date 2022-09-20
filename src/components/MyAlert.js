82ca30587fe9e7d37d038dea24d0616885a26450
darsh297 <88375972+darsh297@users.noreply.github.com>
2022-09-20 12:17:35 +0530

    Update MyAlert.js

--- src/components/MyAlert.js
+++ src/components/MyAlert.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Alert } from "react-bootstrap";
 
-export default function MyAlert(props) {
+ export default function MyAlert(props) {
   return (
     props.alert && <Alert variant={props.alert.type}>{props.alert.message}</Alert>
   );
